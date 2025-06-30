06bdb904145cd451e4b34f954f9a5b5e8f1cd07a
Andreas Olausson <andreas@codepoets.se>
2025-06-30 21:48:44 +0200

    typings

--- leaflet-base/src/main.ts
+++ leaflet-base/src/main.ts
@@ -1,13 +1,18 @@
-import 'leaflet/dist/leaflet.css';
-import 'leaflet-polydraw/dist/styles/polydraw.css';
-import * as L from 'leaflet';
+import "leaflet/dist/leaflet.css";
+import "leaflet-polydraw/dist/styles/polydraw.css";
+import * as L from "leaflet";
 // @ts-ignore
-import Polydraw from 'leaflet-polydraw';
+import Polydraw from "leaflet-polydraw";
 
-const map = L.map('map').setView([58.402514, 15.606188], 13);
+const map = L.map("map").setView([58.402514, 15.606188], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
-new Polydraw().addTo(map);
+const polydraw = new Polydraw().addTo(map);
+
+document.getElementById("addDebugPoly")?.addEventListener("click", () => {
+  console.log("Polydraw:", polydraw);
+});
